4f3589c7811b12e58b1fa319b9c613cae8fe1536
SenseiJon <49255472+SenseiJon@users.noreply.github.com>
2019-11-12 09:05:23 +0900

    Adding exercise.py for week 5

---
+++ week5/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Week 5 Exercises: Testing and Error Handling
+
+# EXERCISE 1: Unit Tests
+
+# Write unit tests two of your week 3 and week 4 exercises.
+# For example, write unit tests for Fizzbuzz and the bubble sort methods.
+# Follow Clean Code standards and commit both your tests and the original
+# code into this folder for easier testing.
+
+# EXERCISE 2: Error Handling
+
+# Write a script that counts the number of instances of each word in a
+# text file. The program should open the file, parse it, and return a
+# dictionary with each word as a key and the number of appearances as
+# a value. You can use the randomtext.txt file for testing.
+
+# Ensure your program has proper error handling. Use Clean Code as your
+# guide. The program should be able to deal with the file missing, or
+# the file not having text, or some other problem. Use try/catch blocks
+# and feel free to break up the program into multiple smaller functions.
